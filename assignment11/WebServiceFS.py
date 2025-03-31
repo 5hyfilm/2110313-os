9cbdf4ece9889cfdfaa6b65ad350974730973ebe
フィルム <waranthorn_c@outlook.com>
2025-03-31 20:08:33 +0700

    .

--- assignment11/WebServiceFS.py
+++ assignment11/WebServiceFS.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 
 import os, stat, errno
-import requests
-
+# pull in some spaghetti to make this stuff work without fuse-py being installed
 try:
     import _find_fuse_parts
 except ImportError:
     pass
-
 import fuse
 from fuse import Fuse
+
+import requests
+
 
 if not hasattr(fuse, '__version__'):
     raise RuntimeError("your fuse-py doesn't know of fuse.__version__, probably it's too old.")
@@ -41,8 +42,7 @@ class WebServiceFS(Fuse):
         elif path == participation_path:
             st.st_mode = stat.S_IFREG | 0o666
             st.st_nlink = 1
-            content = self.myRead(path, 0, 0)
-            st.st_size = len(content)
+            st.st_size = len(self.read(path,0,0))
         else:
             return -errno.ENOENT
         return st
@@ -58,56 +58,38 @@ class WebServiceFS(Fuse):
         if (flags & accmode) not in (os.O_RDONLY, os.O_WRONLY, os.O_RDWR):
             return -errno.EACCES
 
-    def myRead(self, path, size, offset):
-        if path != participation_path:
-            return -errno.ENOENT
-        try:
-            req = requests.get('https://mis.cp.eng.chula.ac.th/krerk/teaching/2022s2-os/status.php')
-            content = req.text.encode("utf-8")
-            if offset < len(content):
-                return content[offset:offset+size]
-            else:
-                return b""
-        except Exception as e:
-            print("myRead error:", e)
-            return b""
-
     def read(self, path, size, offset):
-        return self.myRead(path, size, offset)
-
-    def myWrite(self, path, buf, offset):
         if path != participation_path:
             return -errno.ENOENT
+        
+        req=requests.get('https://mis.cp.eng.chula.ac.th/krerk/teaching/2022s2-os/status.php')
+        content = bytes(req.text, "utf-8")
 
-        try:
-            raw = buf.decode("utf-8").strip().split(':')
-            if len(raw) != 3:
-                return -errno.EINVAL
-
-            checkInUrl = 'https://mis.cp.eng.chula.ac.th/krerk/teaching/2022s2-os/checkIn.php'
-            params = {
-                'studentid': raw[0],
-                'name': raw[1],
-                'email': raw[2]
-            }
-            requests.post(checkInUrl, data=params)
-            return len(buf)
-        except Exception as e:
-            print("myWrite error:", e)
-            return -errno.EIO
+        return content
 
     def write(self, path, buf, offset):
-        return self.myWrite(path, buf, offset)
+        if path != participation_path:
+            return -errno.ENOENT
+
+        raw=buf.decode("utf-8").split(':')
+        
+        if len(raw) != 3:
+            return -errno.EINVAL
+        
+        checkInUrl='https://mis.cp.eng.chula.ac.th/krerk/teaching/2022s2-os/checkIn.php'
+        params= { 'studentid' : raw[0], 'name' : raw[1], 'email' : raw[2] }
+        rpost=requests.post(checkInUrl, data=params)
+        return len(buf)
 
 def main():
-    usage = """
-Web Service Participation Filesystem
+    usage="""
+Web Service Participation
+
 """ + Fuse.fusage
-    server = WebServiceFS(
-        version="%prog " + fuse.__version__,
-        usage=usage,
-        dash_s_do='setsingle'
-    )
+    server = WebServiceFS(version="%prog " + fuse.__version__,
+                     usage=usage,
+                     dash_s_do='setsingle')
+
     server.parse(errex=1)
     server.main()
 
